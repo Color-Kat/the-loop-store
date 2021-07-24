23f678d1d9196cf185f89fd51c35a25ae88af0bb
Color-Kat <50361931+KatSem7@users.noreply.github.com>
2021-07-24 21:48:35 +0300

    main page full screen with slider

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+import Swiper, {
+    Pagination,
+    Autoplay
+} from 'swiper';
+import 'swiper/swiper-bundle.css';
 import dropDownMenu from './modules/dropDownMenu';
 import elementMoveTo from './modules/elementMoveTo';
 import toggleMenu from './modules/toggleMenu';
@@ -8,4 +13,28 @@ dropDownMenu();
 touch_class();
 elementMoveTo();
 
+// click on menu burder to open mobile menu
 document.querySelector('.menu__icon').addEventListener('click', toggleMenu);
+
+// configure Swiper to use modules
+Swiper.use([Pagination, Autoplay]);
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    // direction: 'gorizontal',
+    loop: true,
+    autoplay: {
+        delay: 5000,
+    },
+
+    // // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+    },
+
+    // // Navigation arrows
+    // navigation: {
+    //     nextEl: '.swiper-button-next',
+    //     prevEl: '.swiper-button-prev',
+    // },
+});
